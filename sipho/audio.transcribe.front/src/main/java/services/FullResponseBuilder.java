9f233bd9edcc5f0b91e579c5c52d223b99565bed
Hywel Griffiths <hywel.griffiths@dvsa.gov.uk>
2019-10-05 12:12:48 +0100

    http request complete

---
+++ sipho/audio.transcribe.front/src/main/java/services/FullResponseBuilder.java
@@ -0,0 +1,12 @@
+package services;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+public class FullResponseBuilder {
+    public static String getFullResponse(HttpURLConnection con) throws IOException {
+        StringBuilder fullResponseBuilder = new StringBuilder();
+
+        return fullResponseBuilder.toString();
+    }
+}
